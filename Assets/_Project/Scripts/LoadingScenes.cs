979e2aa784a27db18620a8b313efe6b8ba7bd0b0
Sta-ces <staescedric@gmail.com>
2018-01-26 22:12:38 +0100

    Menu

---
+++ Assets/_Project/Scripts/LoadingScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScenes : MonoBehaviour {
+
+	public void LoadLevel(string _sceneToLoad)
+    {
+        SceneManager.LoadScene(_sceneToLoad);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_Project/Scripts/UIMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenu : MonoBehaviour {
+
+	public void GrowUIText(float _percentToGrow)
+    {
+        gameObject.transform.localScale += Vector3.one * _percentToGrow;
+    }
+
+    public void NormalScaleUIText()
+    {
+        gameObject.transform.localScale = Vector3.one;
+    }
+}

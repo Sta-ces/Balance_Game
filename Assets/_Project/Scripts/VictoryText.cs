d89abf834e55cde990049e57cfbb3e89f54c1823
Sta-ces <staescedric@gmail.com>
2018-01-27 03:16:42 +0100

    New Design

---
+++ Assets/_Project/Scripts/VictoryText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryText : MonoBehaviour {
+
+	public void VictoryTextSetActive()
+    {
+        Debug.Log(gameObject.activeSelf);
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        VictoryTextSetActive();
+    }
+}
